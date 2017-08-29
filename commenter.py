1be1e8cd062e32b72185531a5ffa956a5fc30e68
Sam Marder <sam@marder.co>
2017-08-28 22:45:41 -0400

    Persistence!

--- commenter.py
+++ commenter.py
@@ -2,10 +2,21 @@
 class Comments(object):
     def __init__(self, filename):
         self.comments = {}
-        pass
+        self.savefile = filename + '.comments'
+        try:
+            with open(self.savefile) as f:
+                for line in f:
+                    address, comment = line.split(' ', 1)
+                    self.comments[int(address)] = comment.strip()
+        except FileNotFoundError:
+            pass
+        print('Loaded %d comments' % len(self.comments))
 
     def add_comment(self, address, comment):
+        comment = comment.strip()
         self.comments[int(address)] = comment
+        with open(self.savefile, 'a+') as f:
+            f.write('%s %s\n' % (address, comment))
 
     def get_comment(self, address):
         return self.comments.get(address)
